cc8cc5274fe416f69ed48970325dae5224869a7d
Agustin Serrano <agustds@gmail.com>
2023-04-20 01:24:44 -0300

    Esqueleto

---
+++ src/instagram/Multimedia.java
@@ -0,0 +1,17 @@
+/*Cree la clase multimedia pq las clases audio y video comparten los metodos
+avanzar y detener pero imagen no lo necesita. Entonces los defino como abstractos
+y los redefino en las clases hijas(audio y video). Imagen heredaria de publicaion*/
+package instagram;
+
+public abstract class Multimedia extends Publicacion {
+	private int duracion;//pq lo comparten audio y video
+	 public Multimedia(String nombrePublicacion, String fechaSubida, int cantMG, int duracion) {
+	    super(nombrePublicacion, fechaSubida, cantMG);
+	    this.duracion = duracion;
+	 }
+	
+	public abstract void avanzar();
+	public abstract void detener();
+	
+
+}
---
+++ src/instagram/PerfilInstagram.java
@@ -0,0 +1,13 @@
+package instagram;
+import java.util.*;
+public class PerfilInstagram {
+	private ArrayList<Publicacion> listaPublicaciones;
+	
+	public PerfilInstagram() {
+	    this.listaPublicaciones = new ArrayList<Publicacion>();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
