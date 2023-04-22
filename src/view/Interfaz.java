ff5f5a871303989c56241ffbcbe942c24615ade9
Bianca <biancaiba11@gmail.com>
2023-04-22 10:30:25 -0300

    Se agrego en Interfaz.java un btnAgregarPublicacion, en forma de prueba

--- src/view/Interfaz.java
+++ src/view/Interfaz.java
@@ -7,6 +7,10 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.border.MatteBorder;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Interfaz extends JFrame {
 
@@ -35,12 +39,22 @@ public class Interfaz extends JFrame {
 		setForeground(Color.DARK_GRAY);
 		setBackground(Color.GRAY);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 607, 401);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.GRAY);
 		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 
 		setContentPane(contentPane);
+		
+		JButton btnAgregarPublicacion = new JButton("Agregar Publicación");
+		btnAgregarPublicacion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnAgregarPublicacion.setBackground(Color.LIGHT_GRAY);
+		btnAgregarPublicacion.setFont(new Font("Lucida Sans", Font.PLAIN, 18));
+		contentPane.add(btnAgregarPublicacion);
 	}
 
 }
