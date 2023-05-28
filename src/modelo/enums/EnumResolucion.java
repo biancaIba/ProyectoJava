d69eb7bc773160937475a0855d1bfd1024d6a454
Bianca <111663887+biancaIba@users.noreply.github.com>
2023-05-27 22:42:09 -0300

    comentarios en modelo.enums, modelo.interfaces y sistema

--- src/modelo/enums/EnumResolucion.java
+++ src/modelo/enums/EnumResolucion.java
@@ -1,10 +1,21 @@
 package modelo.enums;
 
+/**
+ * El Enum EnumResolucion.
+ */
 public enum EnumResolucion {
+
+	/** Resolucion 8k. */
 	_8K,
+	/** Resolucion 4k. */
 	_4K,
+	/** Resolucion 2k. */
 	_2K,
+	/** Resolucion fullhd. */
 	FULLHD,
+	/** Resolucion hd. */
 	HD,
+	/** Resolucion sd. */
 	SD
+	
 }
--- src/modelo/interfaces/IDurable.java
+++ src/modelo/interfaces/IDurable.java
@@ -1,16 +1,53 @@
-/**
- * 
- */
 package modelo.interfaces;
 
 import excepciones.DuracionInvalidaExcepcion;
 
-
+/**
+ * La Interface Durable.
+ */
 public interface IDurable {
+
+	/**
+	 * Avanzar.
+	 *
+	 * @param inicioRelativo
+	 * @throws DuracionInvalidaExcepcion
+	 */
 	void avanzar(float inicioRelativo) throws DuracionInvalidaExcepcion;
+
+	/**
+	 * Detener.
+	 *
+	 * @param finRelativo
+	 * @throws DuracionInvalidaExcepcion
+	 */
 	void detener(float finRelativo) throws DuracionInvalidaExcepcion;
+
+	/**
+	 * Obtiene el inicio.
+	 *
+	 * @return el inicio
+	 */
 	float getInicio();
+
+	/**
+	 * Obtiene el fin.
+	 *
+	 * @return el fin
+	 */
 	float getFin();
+
+	/**
+	 * Obtiene el fin de reproduccion.
+	 *
+	 * @return el fin reproduccion
+	 */
 	float getFinReproduccion();
+
+	/**
+	 * Obtiene la duracion.
+	 *
+	 * @return the duracion
+	 */
 	float getDuracion();
 }
